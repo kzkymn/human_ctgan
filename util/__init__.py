274d6eabdc939082ea80fd862748cfb45db7dfc5
kzkymn <kazyama@gmail.com>
2022-06-21 19:18:08 +0900

    updated the libraries

--- util/__init__.py
+++ util/__init__.py
@@ -3,5 +3,6 @@ from .feedback import (create_random_feedback,
                        create_feedback_from_csv,
                        create_feedback_by_one_class_svm,
                        create_feedback_by_knn,
-                       create_feedback_by_knn_with_proba)
+                       create_feedback_by_knn_with_proba,
+                       create_feedback_function_by_rule_base)
 from .output import print_diff_between_original_and_generated_data
--- util/feedback.py
+++ util/feedback.py
@@ -8,7 +8,8 @@ def create_random_feedback(data_for_feedback):
 
 def create_wrong_feedback(data_for_feedback,
                           target_colname='target'):
-    res = [1 if item == 0 else 0 for item in data_for_feedback[target_colname]]
+    # res = [1 if item == 0 else 0 for item in data_for_feedback[target_colname]]
+    res = [1 for _ in data_for_feedback[target_colname]]
     return np.array(res)
 
 
@@ -56,3 +57,44 @@ def create_feedback_by_knn_with_proba(data_for_feedback,
     res = pred_proba_of_target[np.arange(
         len(pred_proba_of_target)), target_labels]
     return np.array(res)
+
+
+def _create_criteria(criteria_df, target_colname, df, df_type='max'):
+    all_criteria_list = []
+    colnames = criteria_df.columns
+    for i in range(criteria_df.shape[0]):
+        criteria_list_for_one_target_value = []
+        for colname in colnames:
+            if colname == target_colname:
+                criterion = (df[colname] == criteria_df[colname].iloc[i])
+            else:
+                if df_type == 'max':
+                    criterion = (df[colname] <= criteria_df[colname].iloc[i])
+                else:
+                    criterion = (df[colname] >= criteria_df[colname].iloc[i])
+            criteria_list_for_one_target_value.append(criterion)
+        all_criteria_list.append(criteria_list_for_one_target_value)
+
+    all_criteria = None
+    for criteria_list_for_one_target_value in all_criteria_list:
+        one_target_criterion = None
+        for criterion in criteria_list_for_one_target_value:
+            if one_target_criterion is None:
+                one_target_criterion = criterion
+            else:
+                one_target_criterion = one_target_criterion & criterion
+        if all_criteria is None:
+            all_criteria = one_target_criterion
+        else:
+            all_criteria = all_criteria | one_target_criterion
+
+    return all_criteria
+
+
+def create_feedback_function_by_rule_base(df, max_df, min_df, target_colname):
+    max_criteria = _create_criteria(
+        max_df, target_colname, df, df_type='max')
+    min_criteria = _create_criteria(
+        min_df, target_colname, df, df_type='min')
+    res = max_criteria & min_criteria
+    return np.array([1 if bool_val else 0 for bool_val in res])
